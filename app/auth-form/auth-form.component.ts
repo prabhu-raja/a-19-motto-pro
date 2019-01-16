98ac1aa8becc6447588c8f6295158237b98b2f75
prabhu-raja <prabhuraja@gmail.com>
2019-01-16 14:07:10 -0500

    End of view child

--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,6 +1,11 @@
-import { Component, Output, EventEmitter, ContentChild, AfterContentInit } from '@angular/core';
-
+import { 
+  Component, Output, EventEmitter, 
+  ContentChild, AfterContentInit,
+  ViewChild, AfterViewInit, 
+  ChangeDetectorRef } from '@angular/core';
+  
 import { AuthRememberComponent } from "./auth-remember.component";
+import { AuthMessageComponent } from './auth-message.component';
 import { User } from './auth-form.interface';
 
 @Component({
@@ -18,25 +23,49 @@ import { User } from './auth-form.interface';
           <input type="password" name="password" ngModel>
         </label>
         <ng-content select="auth-remember"></ng-content>
-        <div *ngIf="showMsg"> U will logged in for 90 days</div>
+
+        <auth-message [style.display]="(showMsg ? 'inherit' : 'none')"></auth-message>
+
         <ng-content select="button"></ng-content>
       </form>
     </div>
   `
 })
-export class AuthFormComponent implements AfterContentInit {
+export class AuthFormComponent implements AfterContentInit, AfterViewInit {
 
   showMsg: boolean = false;
 
+  @ViewChild(AuthMessageComponent) vcMessage: AuthMessageComponent;
   @ContentChild(AuthRememberComponent) ccRemember: AuthRememberComponent;
-  
+
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
 
+  constructor(private cdr: ChangeDetectorRef) { }
+  
+  ngAfterViewInit() {
+    // this.vcMessage.days= 30;
+
+    /*
+    setTimeout(() => {
+      this.vcMessage.days = 30;
+    });
+    */
+    this.vcMessage.days = 30;
+    this.cdr.detectChanges(); // calling Angular to detect chnages
+  }
+
   ngAfterContentInit() {
+    /*
+    if(this.vcMessage) {
+      this.vcMessage.days= 30;
+    }
+    */
+    //
     if(this.ccRemember) {
       this.ccRemember.checked.subscribe((itm: boolean) => this.showMsg = itm);
     }
   }
+
 
   onSubmit(value: User) {
     this.submitted.emit(value);
