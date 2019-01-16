63d3eb464ad4bc220f54407263a789363ba8cd4d
prabhu-raja <prabhuraja@gmail.com>
2019-01-16 15:17:17 -0500

    dynamic components with help of ViewContainerRef and ComponentFactoryResolver

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, AfterContentInit } from '@angular/core';
 
+import { AuthFormComponent } from "./auth-form/auth-form.component";
 import { User } from './auth-form/auth-form.interface';
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
+    <!--
       <auth-form 
         (submitted)="createUser($event)">
         <h3>Create account</h3>
@@ -13,35 +15,27 @@ import { User } from './auth-form/auth-form.interface';
           Join us
         </button>
       </auth-form>
-      <auth-form 
-        (submitted)="loginUser($event)">
-        <h2>Test from login</h2>
-
-        <h3>Login</h3>
-        <auth-remember
-          (checked)="rememberUser($event)">
-        </auth-remember>
-        <button type="submit">
-          Login
-        </button>
-      </auth-form>
+      -->
+      <div #entry></div>
     </div>
   `
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
 
-  rememberMe: boolean = false;
+  @ViewChild('entry', {read: ViewContainerRef}) vcEntry: ViewContainerRef;
 
-  rememberUser(remember: boolean) {
-    this.rememberMe = remember;
+  constructor(private cfr: ComponentFactoryResolver) { }
+
+  ngAfterContentInit() {
+    const authFormFactory = this.cfr.resolveComponentFactory(AuthFormComponent);
+    const cmpt = this.vcEntry.createComponent(authFormFactory);
   }
+  
 
-  createUser(user: User) {
-    console.log('Create account', user);
-  }
+
 
   loginUser(user: User) {
-    console.log('Login', user, this.rememberMe);
+    console.log('Login', user);
   }
 
 }
--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,11 +1,5 @@
-import { 
-  Component, Output, EventEmitter, 
-  ContentChild, AfterContentInit,
-  ViewChild, AfterViewInit, 
-  ChangeDetectorRef } from '@angular/core';
-  
-import { AuthRememberComponent } from "./auth-remember.component";
-import { AuthMessageComponent } from './auth-message.component';
+import { Component, Output, EventEmitter } from '@angular/core';
+
 import { User } from './auth-form.interface';
 
 @Component({
@@ -13,7 +7,7 @@ import { User } from './auth-form.interface';
   template: `
     <div>
       <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
-        <ng-content select="h3"></ng-content>
+        <h3>{{title}}</h3>
         <label>
           Email address
           <input type="email" name="email" ngModel>
@@ -22,50 +16,17 @@ import { User } from './auth-form.interface';
           Password
           <input type="password" name="password" ngModel>
         </label>
-        <ng-content select="auth-remember"></ng-content>
-
-        <auth-message [style.display]="(showMsg ? 'inherit' : 'none')"></auth-message>
-
-        <ng-content select="button"></ng-content>
+        <button type="submit">
+          Submit
+        </button>
       </form>
     </div>
   `
 })
-export class AuthFormComponent implements AfterContentInit, AfterViewInit {
-
-  showMsg: boolean = false;
-
-  @ViewChild(AuthMessageComponent) vcMessage: AuthMessageComponent;
-  @ContentChild(AuthRememberComponent) ccRemember: AuthRememberComponent;
+export class AuthFormComponent {
+  title: string = 'Login';
 
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
-
-  constructor(private cdr: ChangeDetectorRef) { }
-  
-  ngAfterViewInit() {
-    // this.vcMessage.days= 30;
-
-    /*
-    setTimeout(() => {
-      this.vcMessage.days = 30;
-    });
-    */
-    this.vcMessage.days = 30;
-    this.cdr.detectChanges(); // calling Angular to detect chnages
-  }
-
-  ngAfterContentInit() {
-    /*
-    if(this.vcMessage) {
-      this.vcMessage.days= 30;
-    }
-    */
-    //
-    if(this.ccRemember) {
-      this.ccRemember.checked.subscribe((itm: boolean) => this.showMsg = itm);
-    }
-  }
-
 
   onSubmit(value: User) {
     this.submitted.emit(value);
