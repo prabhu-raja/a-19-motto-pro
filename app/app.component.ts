326f3a63d46e7d15de20474dc56a26ef8fe86319
prabhu-raja <prabhuraja@gmail.com>
2019-01-20 19:12:52 -0500

    test

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,31 +1,22 @@
-import { 
-  Component,
-  ViewChild, ViewContainerRef, AfterContentInit,
-  TemplateRef } from '@angular/core';
-
-import { AuthFormComponent } from "./auth-form/auth-form.component";
-import { User } from './auth-form/auth-form.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
-      <div #entry></div>
+      <ng-container [ngTemplateOutlet]="test ? tmpl : tmpl2">
+      </ng-container>
+      <!-- -->
       <template #tmpl>
         PR, Salem
       </template>
+
+      <template #tmpl2>
+        Prabhu Raja, Brampton
+      </template>
     </div>
-   
   `
 })
-export class AppComponent implements AfterContentInit {
-
-  @ViewChild('entry', {read: ViewContainerRef}) vcEntry: ViewContainerRef;
-  @ViewChild('tmpl') vcTempl: TemplateRef<any>;
-
-  ngAfterContentInit() {
-    this.vcEntry.createEmbeddedView(this.vcTempl);
-    
-  }
-
+export class AppComponent  {
+  test: boolean = true;
 }
