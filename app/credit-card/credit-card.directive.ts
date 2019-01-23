a407712e721687c082420a5a655a4e3efc02834b
prabhu-raja <prabhuraja@gmail.com>
2019-01-23 17:57:10 -0500

    host binding

--- app/credit-card/credit-card.directive.ts
+++ app/credit-card/credit-card.directive.ts
@@ -1,23 +1,33 @@
-import { Directive, HostListener, ElementRef } from "@angular/core";
+import { Directive, HostListener, HostBinding } from "@angular/core";
 
 @Directive({
     selector: '[credit-card]'
 })
 export class CreditCardDirective {
-   @HostListener('input',['$event'])
-   myKeydown(evt: KeyboardEvent) {
-       const ip =evt.target as HTMLInputElement;
-       //
-       let trimmed = ip.value.replace(/\s+/g, '');
-       if(trimmed.length > 16) {
-           trimmed = trimmed.substr(0, 16);
-       }
-       //
-       let numbers = [];
-       for (let i = 0; i < trimmed.length; i+= 4) {
-           numbers.push(trimmed.substr(i, 4));
-       }
-       //
-       ip.value = numbers.join(' ');
-   }
+
+    @HostBinding('style.border')
+    myBorder: string;
+    
+    @HostListener('input',['$event'])
+    myKeydown(evt: KeyboardEvent) {
+        const ip = evt.target as HTMLInputElement;
+        //
+        let trimmed = ip.value.replace(/\s+/g, '');
+        if(trimmed.length > 16) {
+            trimmed = trimmed.substr(0, 16);
+        }
+        //
+        let numbers = [];
+        for (let i = 0; i < trimmed.length; i+= 4) {
+            numbers.push(trimmed.substr(i, 4));
+        }
+        //
+        ip.value = numbers.join(' ');
+
+        //
+        this.myBorder = '';
+        if(/[^\d]+/.test(trimmed)) {
+            this.myBorder = '1px solid red';
+        }
+    }
 }
