e7666336f7c8970f2b9d8945eb4b0ef9cf66925e
prabhu-raja <prabhuraja@gmail.com>
2019-01-23 12:26:54 -0500

    init

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,40 +1,19 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  changeDetection: ChangeDetectionStrategy.Default,
   template: `
     <div>
-      <button (click)="addProp()">Add property</button>
-      <button (click)="changeUser()">Change user object</button>
-      <button (click)="changeName()">Change name property</button>
-      <div class="users">
-        <example-one [user]="user"></example-one>
-        <example-two [user]="user"></example-two>
-      </div>
+      <label>
+        Credit Card Number
+        <input 
+          name="credit-card" 
+          type="text"
+          placeholder="Enter your 16-digit card number"
+          credit-card>
+      </label>
     </div>
   `
 })
 export class AppComponent {
-  user: any = {
-    name: 'Mark Hoppus',
-    age: 44,
-    location: 'California'
-  };
-
-  addProp() {
-    this.user.email = 'blink@blink-182.net';
-  }
-
-  changeName() {
-    this.user.name = 'Travis Barker';
-  }
-
-  changeUser() {
-    this.user = {
-      name: 'Tom Delonge',
-      age: 41,
-      location: 'California'
-    };
-  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ExampleOneComponent } from './one/one.component';
-import { ExampleTwoComponent } from './two/two.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ExampleOneComponent,
-    ExampleTwoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
