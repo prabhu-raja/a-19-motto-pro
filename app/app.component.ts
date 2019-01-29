60c152299b1ef0024c592538fdc3b34b4be2b30b
prabhu-raja <prabhuraja@gmail.com>
2019-01-29 18:25:28 -0500

    28-pipe-providers

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FileSizePipe } from "./filesize.pipe";
 
 interface File {
   name: string,
-  size: number,
+  size: any,
   type: string
 }
 
@@ -10,20 +11,38 @@ interface File {
   selector: 'app-root',
   template: `
     <div>
-      <div *ngFor="let file of files">
+      <div *ngFor="let file of mappedFiles">
         <p>{{ file.name }}</p>
-        <p>{{ file.size | filesize: ' Megabytes' }}</p>
+        <!--<p>{{ file.size | filesize: ' Megabytes' }}</p>-->
+        <p>{{ file.size }}</p>
       </div>
     </div>
-  `
+  `,
+  providers:[ 
+    FileSizePipe
+  ]
 })
 export class AppComponent implements OnInit {
   files: File[];
+  mappedFiles: File[];
+
+  constructor(private sizePipe: FileSizePipe) { }
+
   ngOnInit() {
     this.files = [
       { name: 'logo.svg', size: 2120109, type: 'image/svg' },
       { name: 'banner.jpg', size: 18029, type: 'image/jpg' },
       { name: 'background.png', size: 1784562, type: 'image/png' }
     ];
+    //
+    this.mappedFiles = this.files.map(file => {
+      return { 
+        name: file.name,
+        type: file.type,
+        size: this.sizePipe.transform(file.size, ' mb')
+      }
+    });
   }
+
 }
+``
