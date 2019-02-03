0bd0a3dbc336e392e9e8b80e2db13dd815114576
prabhu-raja <prabhuraja@gmail.com>
2019-02-02 19:44:38 -0500

    first set of reolve completed

--- app/mail/containers/mail-folder/mail-folder.component.ts
+++ app/mail/containers/mail-folder/mail-folder.component.ts
@@ -4,27 +4,31 @@ import { Mail } from '../../models/mail.interface';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/operator/pluck';
+
 @Component({
   selector: 'mail-folder',
   styleUrls: ['mail-folder.component.scss'],
   template: `
-    <h2>Inbox</h2>
-    <p>{{data | json}}</p>
-   <!--
+    <h2>{{title | async}}</h2>
+    <!--p>{{data | json}}</p>-->
+
     <mail-item
-      *ngFor="let message of messages"
+      *ngFor="let message of (propMessages | async)"
       [message]="message">
     </mail-item>
-    -->
   `
 })
 export class MailFolderComponent implements OnInit {
-    
-  data: Observable<{ propMessages: Mail[] }> = this.actRoute.data;
+
+  // data: Observable<{ propMessages: Mail[] }> = this.actRoute.data;
+  propMessages: Observable<Mail[]> = this.actRoute.data.pluck('propMessages');
+  title: Observable<string> = this.actRoute.params.pluck('pname');
 
   constructor(private actRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    
     this.actRoute.params.subscribe(data => console.log('nn', data));
   }
 }
