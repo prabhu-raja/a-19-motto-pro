41c4d745c21fb4533b70874ae8277a27d41f4c2e
prabhu-raja <prabhuraja@gmail.com>
2019-02-03 01:11:58 -0500

    navigating via named outlets

--- app/mail/components/mail-item/mail-item.component.ts
+++ app/mail/components/mail-item/mail-item.component.ts
@@ -5,15 +5,7 @@ import { Mail } from '../../models/mail.interface';
 @Component({
   selector: 'mail-item',
   styleUrls: ['mail-item.component.scss'],
-  template: `
-    <a class="mail-item">
-      <h3>
-        {{ message.from }}
-        <span>{{ message.timestamp | date:'shortTime' }}</span>
-      </h3>
-      <p>{{ message.summary }}</p>
-    </a>
-  `
+  templateUrl: './mail-item.component.html'
 })
 export class MailItemComponent {
   @Input()
