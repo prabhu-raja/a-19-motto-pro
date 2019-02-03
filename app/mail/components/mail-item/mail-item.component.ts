f68d907745606fe6c435c5e8b468c8ac43ef6acf
prabhu-raja <prabhuraja@gmail.com>
2019-02-03 18:45:24 -0500

    created nav api instead of router link

--- app/mail/components/mail-item/mail-item.component.ts
+++ app/mail/components/mail-item/mail-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Mail } from '../../models/mail.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mail-item',
@@ -10,4 +11,19 @@ import { Mail } from '../../models/mail.interface';
 export class MailItemComponent {
   @Input()
   message: Mail;
+
+  constructor(private router: Router) {
+
+  }
+
+  navigateToMessage() {
+    this.router.navigate([
+      '',
+      {
+        outlets: {
+          pane: ["message", this.message.id]
+        }
+      }
+    ]);
+  }
 }
